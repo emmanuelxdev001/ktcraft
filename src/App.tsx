58bca15715a181697422107587d27315c874df49
Emmanuel John <emmanueljohndev247@gmail.com>
2025-02-01 03:43:17 +0100

    first honour fr john

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="bg-gray-800 text-white p-2">
+        <h1>Welcome to KTCRAFT</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
